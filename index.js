c1a0ec26b734f6ac867c079a27828f1fca1c22dd
Alexander Schmitz <arschmitz@gmail.com>
2016-03-15 11:07:10 -0400

    Added grunt tasks for jshint and jscs

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var jsass = require( "jsass-vars" );
 var path = require( "path" );
 module.exports = function( grunt ) {
 	grunt.registerMultiTask( "jsass", "generate scss variable files", function() {
-		this.files.forEach(function( file ) {
+		this.files.forEach( function( file ) {
 			var options = this.options();
-			var paths = file.src.filter(function( filepath ) {
+			var paths = file.src.filter( function( filepath ) {
 
 				// Remove nonexistent files (it's up to you to filter or warn here).
 				if ( !grunt.file.exists( filepath ) ) {
@@ -13,21 +13,16 @@ module.exports = function( grunt ) {
 				} else {
 					return true;
 				}
-			}).map( function(filepath) {
+			} ).map( function( filepath ) {
 
 				// Make src absolute as thats what jsass expects
-				return path.join( process.cwd(), filepath )
-			});
-
-			// Make sure directory exists jsass wont create it
-			if ( !grunt.file.exists( distPath ) {
-				grunt.file.mkdir( distPath );
-			}
+				return path.join( process.cwd(), filepath );
+			} );
 
 			options.dest = file.dest;
 
 			jsass( paths, options );
 			grunt.log.writeln( "File '" + file.dest + "' created. from" + file.src );
-		});
+		} );
 	} );
 };
